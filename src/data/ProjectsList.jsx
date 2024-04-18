f3f5a9447fdfe8c5b5cc3627966790a797c2a167
ajithk1503 <ajithk2004@gmail.com>
2024-04-18 18:52:50 +0530

    Initial commit

---
+++ src/data/ProjectsList.jsx
@@ -0,0 +1,31 @@
+export const ProjectsList = {
+  projects: [
+    {
+      name: "Personal Portfolio",
+      description:
+        "My portfolio is a website built with React.js and Tailwind CSS, utilizing React Router v6 to enable easy access to multiple pages. It was designed and created entirely by myself, showcasing my skills and projects. The website is responsive and visually appealing, providing a user-friendly experience.",
+      technologies: ["React JS", "Tailwind CSS", "Git", "GitHub", "Figma"],
+    },
+    {
+      name: "Dashboard Panel",
+      description:
+        "User Admin Dashboard created with React js, Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid to build this entire application. This application consists of Light and Dark Mode, four different Charts, three different Data Table Pages, FAQ Page, Form Page, and Calendar Integration.",
+      technologies: ["React JS", "Material UI"],
+    },
+    {
+      name: "Netflix-Clone",
+      description:
+        "This project is a simplified front-end clone of Netflix. It was created with React js. It uses TMDB (The Movie Data Base ) API and uses the Axios tool. Users can click movie images with embedded YouTube trailers or related videos about the movie.",
+      technologies: [
+        "React JS",
+        "REST APIs",
+        "axios",
+        "CSS",
+        "Git",
+        "GitHub",
+        "Figma",
+      ],
+    },
+    
+  ],
+};
---
+++ src/data/UserData.jsx
@@ -0,0 +1,30 @@
+export const UserData = {
+  name: "Rahul Vijay",
+  about : "As a self-taught front-end developer, I excel in crafting dynamic, accessible websites across all devices. Specializing in React.js, Redux, JavaScript, HTML5, and CSS, I consistently deliver innovative solutions that enhance user experiences. Proficient in Git version control, I ensure efficient code management and seamless communication. Additionally, my expertise extends to developing apps using mini program studio frameworks, consistently surpassing client expectations with high-caliber solutions. ",
+  resumeUrl : "https://docs.google.com/document/d/1h4j7IoDbAEjwmBYaJvdbH9ipmImOWRaF/edit",
+  FooterLink : "https://www.linkedin.com/in/kanumuri-ajith-varma-00413224b/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/ajithk1503",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/kanumuri-ajith-varma-00413224b/",
+      icon: "FaLinkedinIn",
+    },
+    
+     
+  ],
+  typewriterOptions: {
+    strings: [
+      "Front End Developer",
+      "React Js Developer",
+      "Data Scientist",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
